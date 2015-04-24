2c3192a3318b9e8ee771053e4beda49f144a8fd6
vmtco <vmtco.com@gmail.com>
2015-04-24 09:25:44 +0430

    Update jquery.disableSelection.coffee
    Update jquery.disableSelection.js

--- jquery.disableSelection.coffee
+++ jquery.disableSelection.coffee
@@ -1,12 +1,10 @@
 ( ($) ->
     $.fn.disableSelection = ->
         this.attr('unselectable', 'on').css('user-select', 'none').on('selectstart', false)
-        this.on("DOMNodeInserted", (e) ->
-                    items = $(e.target)
-                    if items.length > 0
-                       items.attr('unselectable', 'on').css('user-select', 'none').on('selectstart', false)
-                    return
-               )
+        this.on "DOMNodeInserted", (e) ->
+                items = $(e.target)
+                items.attr('unselectable', 'on').css('user-select', 'none').on('selectstart', false) if items.length > 0
+                return
         return
     return
 )(jQuery)
--- jquery.disableSelection.js
+++ jquery.disableSelection.js
@@ -1,12 +1,12 @@
-(function ($) {
-    $.fn.disableSelection = function () {
-        this.attr('unselectable', 'on').css('user-select', 'none').on('selectstart', false);
-        this.on("DOMNodeInserted", function (e) {
-            var items;
-            items = $(e.target);
-            if (items.length > 0) {
-                items.attr('unselectable', 'on').css('user-select', 'none').on('selectstart', false);
-            }
-        });
-    };
+(function($) {
+  $.fn.disableSelection = function() {
+    this.attr('unselectable', 'on').css('user-select', 'none').on('selectstart', false);
+    this.on("DOMNodeInserted", function(e) {
+      var items;
+      items = $(e.target);
+      if (items.length > 0) {
+        items.attr('unselectable', 'on').css('user-select', 'none').on('selectstart', false);
+      }
+    });
+  };
 })(jQuery);
